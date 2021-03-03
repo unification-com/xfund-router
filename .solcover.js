415dd699d1220ab05fe9e9b1eb6fcb692b7554c6
Codegnosis <paul@codegnosis.co.uk>
2021-03-03 13:09:27 +0000

    coverage - set gasPrice and increase default balance for coverage tests

--- .solcover.js
+++ .solcover.js
@@ -6,7 +6,9 @@ module.exports = {
     "mocks/MockConsumerCustomRequest.sol",
     "mocks/MockToken.sol"],
   providerOptions: {
-    mnemonic: "myth like bonus scare over problem client lizard pioneer submit female collect"
+    mnemonic: "myth like bonus scare over problem client lizard pioneer submit female collect",
+    gasPrice: "0x4A817C800",
+    default_balance_ether: 100000,
   },
   mocha: {
     enableTimeouts: false
