d7b066df72185807e455009528aff15f900af9e4
Codegnosis <paul@codegnosis.co.uk>
2021-04-21 11:08:18 +0100

    migrations - update deployment script

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -1,6 +1,5 @@
 const MockToken = artifacts.require("MockToken")
 const Router = artifacts.require("Router")
-const ConsumerLib = artifacts.require("ConsumerLib")
 const MockConsumer = artifacts.require("MockConsumer")
 const MockConsumerCustomRequest = artifacts.require("MockConsumerCustomRequest")
 
@@ -18,32 +17,23 @@ module.exports = function (deployer, network) {
           return deployer.deploy(Router, MockToken.address)
         })
         .then(function () {
-          // 3. ConsumerLib
-          return deployer.deploy(ConsumerLib)
+          // 3. MockConsumer
+          return deployer.deploy(MockConsumer, Router.address, MockToken.address)
         })
         .then(function () {
-          // 4. MockConsumer
-          deployer.link(ConsumerLib, MockConsumer)
-          return deployer.deploy(MockConsumer, Router.address)
-        })
-        .then(function () {
-          deployer.link(ConsumerLib, MockConsumerCustomRequest)
-          return deployer.deploy(MockConsumerCustomRequest, Router.address)
+          // 4. MockConsumerCustomRequest
+          return deployer.deploy(MockConsumerCustomRequest, Router.address, MockToken.address)
         })
       break
     case "rinkeby":
     case "rinkeby-fork":
-      // 1. Router
-      deployer.deploy(Router, "0x245330351344f9301690d5d8de2a07f5f32e1149").then(function () {
-        // 2. ConsumerLib
-        return deployer.deploy(ConsumerLib)
-      })
+      // Router
+      deployer.deploy(Router, "0x245330351344f9301690d5d8de2a07f5f32e1149")
       break
     case "mainnet":
-      deployer.deploy(Router, "0x892A6f9dF0147e5f079b0993F486F9acA3c87881").then(function () {
-        // 2. ConsumerLib
-        return deployer.deploy(ConsumerLib)
-      })
+    case "mainnet-fork":
+      // Router
+      deployer.deploy(Router, "0x892A6f9dF0147e5f079b0993F486F9acA3c87881")
       break
   }
 }
