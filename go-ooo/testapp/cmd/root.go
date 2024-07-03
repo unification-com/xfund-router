49ddc0a42f5abec5b3eaa9c77f730997633cf776
Codegnosis <paul@codegnosis.co.uk>
2024-07-03 10:44:24 +0100

    go-ooo - update testapp

--- go-ooo/testapp/cmd/root.go
+++ go-ooo/testapp/cmd/root.go
@@ -1,6 +1,8 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"github.com/spf13/cobra"
+)
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -11,5 +13,6 @@ var rootCmd = &cobra.Command{
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
+	rootCmd.PersistentFlags().StringVar(&graphNetApi, FlagGraphNetworkApiKey, "", "Graph Network API Key")
 	cobra.CheckErr(rootCmd.Execute())
 }
--- go-ooo/utils/utils.go
+++ go-ooo/utils/utils.go
@@ -85,3 +85,13 @@ func EtherToWei(eth *big.Float) *big.Int {
 	wei := new(big.Int).Add(truncInt, fracInt)
 	return wei
 }
+
+func WeiToEther(wei *big.Int) *big.Float {
+	f := new(big.Float)
+	f.SetPrec(236) //  IEEE 754 octuple-precision binary floating-point format: binary256
+	f.SetMode(big.ToNearestEven)
+	fWei := new(big.Float)
+	fWei.SetPrec(236) //  IEEE 754 octuple-precision binary floating-point format: binary256
+	fWei.SetMode(big.ToNearestEven)
+	return f.Quo(fWei.SetInt(wei), big.NewFloat(params.Ether))
+}
